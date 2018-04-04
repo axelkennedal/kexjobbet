f0054d850e877a0ef29defd06e69e3a24381f6a0
Nicklas Hersén <nhersen@kth.se>
2018-04-04 20:10:55 +0200

    Tweak SVC parameters

--- playground/final/classify.py
+++ playground/final/classify.py
@@ -4,47 +4,44 @@ import sklearn.svm as svm
 import numpy as np
 from sklearn.preprocessing import normalize
 
-dataBaseDir = '../../../kexdata/'
+dataBaseDir = '../../../data/'
 
 trainingData = { 'data': [], 'group': [] }
 songHashGenreName = {}
 
+print('Extracting features...')
 ## Generate sample, feature vector matrix (training data)
-for filename in glob.glob(dataBaseDir + 'training/*.mp3'):
-	featureVector = get_feature_vector(filename)
-	fileGenre = get_genre(filename)
+filenames = glob.glob(dataBaseDir + 'training/*.mp3')
 
-	## Discard data where the dimension of the feature vector does not match the dimension
-	## of the other feature vectors.
-	if (trainingData['data'] != []):
-		if(len(trainingData['data'][-1]) != len(featureVector)):
-			continue
+trainingData, songHashGenreName = extract_feature_and_class(filenames)
+data = np.array(trainingData['data'])
+group = np.array(trainingData['group'])
 
-	genreHash = hash(fileGenre)
-	if (genreHash not in songHashGenreName):
-		songHashGenreName.update({genreHash: fileGenre})
+data.tofile('mfccdata.dat')
+group.tofile('gdata.dat')
+# data = np.fromfile('mfccdata.dat', dtype = float).reshape(125,12)
+# group = np.fromfile('gdata.dat', dtype = int)
 
-	trainingData['data'].append(featureVector)
-	trainingData['group'].append(genreHash)
 
+print('\nTraining classifier...')
 # Generate classifier model
-classifier = svm.SVC(kernel = 'rbf', gamma = 'auto', C = 1)
-classifier.fit(normalize(np.array(trainingData['data']), axis=0), np.array(trainingData['group']))
-print(classifier.score(normalize(np.array(trainingData['data']), axis=0), np.array(trainingData['group'])))
+classifier = svm.SVC(kernel = 'rbf', gamma = 1/10, C = 100, tol = 0.00001)
+classifier.fit(data, group)
+print('Score: %f' % classifier.score(data, group))
 
 # Attempt to predict data
-total = 0;
-hit = 0;
+total = 0
+hit = 0
 for filename in glob.glob(dataBaseDir + 'classify/*.mp3'):
-	expected = hash(get_genre(filename))
-	result = classifier.predict(np.array(get_feature_vector(filename)).reshape(1, -1))
+    expected = hash(get_genre(filename))
+    result = classifier.predict(np.array(get_feature_vector(filename)).reshape(1, -1))
 
-	if (expected == result):
-		hit += 1
-	else:
-		print ("Classified %s as %s\n\tSong: %s" % (songHashGenreName[expected], songHashGenreName[list(result)[0]], filename))
+    if (expected == result[0]):
+        hit += 1
+    else:
+        print ("Classified %s as %s\n\tSong: %s" % (songHashGenreName[expected], songHashGenreName[list(result)[0]], filename))
 
-	total += 1
+    total += 1
 
 print("\n\nFinished classifying %d songs." % total)
 print("Accuracy {}% [{}/{}]".format(100 * hit / total, hit, total))
--- playground/final/extract.py
+++ playground/final/extract.py
@@ -27,7 +27,6 @@ def get_genre(filename):
 ##      np.array(featureVector) - based on mfcc (normalized)
 def get_feature_vector(filename):
 	featureVector = []
-
 	fileData, sr = librosa.load(filename, sr = sampleRate, duration = duration, offset = offset)
 
 	mfccs = librosa.feature.mfcc(fileData, sampleRate, n_mfcc = nMfcc) 
@@ -36,3 +35,34 @@ def get_feature_vector(filename):
 		featureVector.append(sum(tmplist) / len(tmplist))
 
 	return featureVector
+
+## Extract the features and genre from the specified files.
+## Input:
+##      filenames - list of filenames to process
+##
+## Output: { 'data': [], 'group': [] }
+##      Dictionary containing the features associated with each file
+##      and the associated class.
+##
+def extract_feature_and_class(filenames):
+    songHashGenreName = {}
+    trainingData = { 'data': [], 'group': [] }
+
+    for filename in filenames:
+        featureVector = get_feature_vector(filename)
+        fileGenre = get_genre(filename)
+
+        ## Discard data where the dimension of the feature vector does not match the dimension
+        ## of the other feature vectors.
+        if (trainingData['data'] != []):
+            if(len(trainingData['data'][-1]) != len(featureVector)):
+                continue
+
+        genreHash = hash(fileGenre)
+        if (genreHash not in songHashGenreName):
+            songHashGenreName.update({genreHash: fileGenre})
+
+        trainingData['data'].append(featureVector)
+        trainingData['group'].append(genreHash)
+
+    return trainingData, songHashGenreName
