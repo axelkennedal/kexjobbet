c8765e3791f30b76db5f7f2830603d81a8230688
Nicklas Hersén <nicklas.hersen@gmail.com>
2018-04-20 15:05:14 +0200

    Add automatic CI generation

--- playground/final/run_repeated_tests.py
+++ playground/final/run_repeated_tests.py
@@ -3,6 +3,8 @@ from random import shuffle
 from sklearn.svm import SVC
 from mutagen.id3 import ID3
 import warnings
+import scipy.stats as stats
+from extract import *
 
 ## Generates a pair list from a dictionary containing
 ## the various mfccs and genre hashes. Allows us to shuffle the
@@ -10,7 +12,7 @@ import warnings
 ##
 ## Input:
 ##      data - {'data': [mfccs], 'group': groups} (output from 
-##              generate_feature_vector)
+##              generate_feature_vector(...)[])
 ##      
 ## Output:
 ##      pairList - A list of each mfcc/genre association [(mfccs, genre)]
@@ -87,4 +89,48 @@ def run_repeated_tests(pairList, nIter):
         
     return iterationResults
         
+## Calculate the confidence interval mode2-mode1 for each specified duration
+## using Welch's t-test.
+##
+## Input:
+##      filenames - files to extract features from
+##      durations - durations to test [5, ...]
+##      nIter     - iterations on each duration to run
+##      mode1     - OffsetMode1
+##      mode2     - OffsetMode2 
+##
+## Output:
+##      dict containing the 99.9% CI for each duration
+##      {duraiton1: (lower, upper), duration2:... }
+def CI_interval(filenames, durations, nIter, mode1, mode2):
+    res = {}
+
+    for duration in durations:
+        pairList1 = generate_pair_list(generate_feature_vector(filenames, 8, mode1, duration)[0])
+        pairList2 = generate_pair_list(generate_feature_vector(filenames, 8, mode2, duration)[0])
+
+        results1 = run_repeated_tests(pairList1, nIter)
+        results2 = run_repeated_tests(pairList2, nIter)
+        mean1 = np.mean(results1)
+        mean2 = np.mean(results2)
+
+        # t^2 for results1 and results2
+        t1 = 1/(len(results1) - 1) * np.sum(list(map(lambda x: (x - mean1)**2, results1)))
+        t2 = 1/(len(results2) - 1) * np.sum(list(map(lambda x: (x - mean2)**2, results2)))
+
+        meanAcc = np.mean(np.array(results2) - np.array(results1))
+
+        # t for pairList2 - pairList1
+        tDiff = np.sqrt(t1/len(results1) + t2/len(results1))
+
+        # Calculate degrees of freedom (Welch's test)
+        tj = (t1/len(results1) + t2/len(results2))**2
+        nm = (t1**2/((len(results1)**2) * (len(results1)-1))) + (t2**2/((len(results2)**2) * (len/results2)-1))
+        tX = stats.t.ppf(0.9995, tj/nm)
+
+        v = tX/np.sqrt(len(results1 + results2))*tDiff
+
+        res.update({duration: (meanAcc - v, meanAcc + v)})
+
+    return res
 
